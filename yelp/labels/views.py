6a9ac457d482ac4a2b18571e822bc5a5e41a1cd4
athiradas <athiradas.mec@gmail.com>
2017-03-05 00:45:28 -0600

    Inception 7 model test

--- yelp/labels/views.py
+++ yelp/labels/views.py
@@ -1,6 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 27 17:23:46 2017
+
+@author: athir
+"""
+
+'''
+Using a pretrained net to generate features for images
+Assuming the script is run on a GPU, it takes close to 6 hours to complete (This can be optimized by changing batch size)
+The output files are close to 4GB each for train and test
+The inception net weights and architecture need to be downloaded from here: https://github.com/dmlc/mxnet-model-gallery/blob/master/imagenet-1k-inception-v3.md
+'''
+
+
+import mxnet as mx
+import logging
+import numpy as np
+from skimage import io, transform
+from mxnet import model
+import pandas as pd
+import time
+import datetime
+
 from django.http import HttpResponse
 from .models import Preprocess
 from django.template import loader, RequestContext
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+bs = 1
 
 def index(request):
         preprocess = Preprocess()
