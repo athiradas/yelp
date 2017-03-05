5e5436897bf7de33b20b56757bb8cdad86527e76
Ubuntu <ubuntu@ip-172-31-10-152.us-west-2.compute.internal>
2017-03-05 05:59:20 +0000

    Django Basics Push

--- yelp/labels/models.py
+++ yelp/labels/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Preprocess(models.Model):
+	def inception_v3(self):
+		return 'inception_v3'
+
--- yelp/labels/views.py
+++ yelp/labels/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
-
+from .models import Preprocess
+from django.template import loader, RequestContext
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+        preprocess = Preprocess()
+        preprocess_iv3 = preprocess.inception_v3()
+        template = loader.get_template('labels/index.html')
+        context = {'preprocess_iv3':preprocess_iv3}
+        return HttpResponse(template.render(context, request))
+
